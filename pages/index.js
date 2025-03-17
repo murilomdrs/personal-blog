559ff73381abf51ad1548b49a1663b9a1dd2cc29
Murilo <murilomdrs@gmail.com>
2025-03-17 21:24:33 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Tô tisti, quer fazer um PIX pra mim? Pode ser R$1.000,00.</h1>
+  return <h1>Tô tisti, quer fazer um PIX pra mim? Pode ser R$1.000,00.</h1>;
 }
 
 export default Home;
