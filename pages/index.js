9a83bb1cae317365739797223117f2c28dd183c2
Murilo <murilomdrs@gmail.com>
2025-03-18 20:03:27 +0000

    acrescenta botão `buymeacoffee` na página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 function Home() {
-  return <h1>Tô tisti, quer fazer um PIX pra mim? Pode ser R$1.000,00.</h1>;
+  return (
+    <h1>
+      Tô tisti, quer fazer um PIX pra mim? Pode ser R$1.000,00.
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="murilomedeiros"
+        data-description="Support me on Buy me a coffee!"
+        data-message=""
+        data-color="#5F7FFF"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+      ></script>
+    </h1>
+  );
 }
 
 export default Home;
